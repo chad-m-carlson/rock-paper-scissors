a5600c52387690a9f4d39ef66370b4df806a6999
Chad Carlson <chad.m.carlson@gmail.com>
2019-04-30 21:46:21 -0600

    fixed some bugs, added draw tally counter

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let userScore = 0;
 let computerScore = 0;
 let userWins = 0;
 let computerWins = 0;
+let numberOfDraws = 0;
 const buttons = document.querySelectorAll('.gameButton');
 const playButton = document.getElementById('play');
 const restart = document.querySelector('#restart');
@@ -10,6 +11,7 @@ const score = document.querySelector('#score');
 const plays = document.querySelector('#plays');
 const computer = document.getElementById('computer');
 const user = document.getElementById('user');
+const drawGame = document.getElementById('draw');
 
 buttons.forEach((gamebutton) => gamebutton.disabled = true);
 
@@ -38,14 +40,16 @@ function hideInputs() {
 }
 
 function restartClicked() {
-    buttons.forEach((gamebutton)=> gamebutton.disabled = true);
+    buttons.forEach((gamebutton) => gamebutton.disabled = true);
     hideInputs();
     resetGame();
     removeLogItems();
     user.removeChild(user.firstChild);
     computer.removeChild(computer.firstChild);
+    drawGame.removeChild(draw.firstChild);
     userWins = 0;
     computerWins = 0;
+    numberOfDraws = 0;
 }
 
 function computerPlay() {
@@ -57,13 +61,13 @@ function playRound(playerSelection) {
     numberOfGames = document.getElementById('games').value;
     const computerSelection = computerPlay();
     if ((playerSelection == computerSelection)) {
-        draw();
+        gameResults('draw');
     } else if (playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'scissors' && computerSelection == 'rock' || playerSelection == 'paper' && computerSelection == 'scissors') {
         computerScore++;
-        computerGame();
+        gameResults('computer');
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
         userScore++;
-        playerGame();
+        gameResults('player');
     }
     compPlay();
     checkResults();
@@ -75,34 +79,45 @@ function checkResults() {
             const dd = document.createElement('dd');
             dd.innerHTML = `You win! ${userScore} to ${computerScore}.`;
             score.appendChild(dd);
-            userWins += 1;
+            userWins++;
         } else if (computerScore > userScore) {
             const dd = document.createElement('dd');
             dd.innerHTML = `You lose! ${computerScore} to ${userScore}`;
             score.appendChild(dd);
-            computerWins += 1;
+            computerWins++;
         } else {
             const dd = document.createElement('dd');
             dd.innerHTML = `It's a tie! Please play again!`;
             score.appendChild(dd);
+            numberOfDraws++;
         }
         resetGame();
         disableButton();
         writeScores();
-        setTimeout(function() { rounds();}, 2000);
+        setTimeout(function () {
+            rounds();
+        }, 2000);
     }
 }
 
 function writeScores() {
     computer.innerHTML = `Computer Score: ${computerWins}`;
     user.innerHTML = `Player Score: ${userWins}`;
+    drawGame.innerHTML = `Draw Game: ${numberOfDraws}`;
 }
 
 function rounds() {
     const numberOfRounds = document.getElementById('rounds').value;
     if (computerWins + userWins == numberOfRounds) {
-        if (computerWins > userWins) alert('Game Over! Computer wins the game');
-        else alert('Congratulations! You win the game');
+        if (computerWins > userWins) {
+            alert('Game Over! Computer wins the game');
+        } else if (computerWins < userWins) {
+            alert('Congratulations! You Win the Game');
+        } else if (computerWins === userWins) {
+            alert('It\'s a tie');
+        }
+        resetRounds();
+        hideInputs();
     }
 }
 
@@ -132,22 +147,22 @@ function removeLogItems() {
     }
 }
 
-function draw() {
-    const dd = document.createElement('dd');
-    dd.innerHTML = `It's a tie, you both played ${playerClicked}.`;
-    log.appendChild(dd);
-}
-
-function computerGame() {
-    const dd = document.createElement('dd');
-    dd.innerHTML = `You Lose! ${computerChoice} beats ${playerClicked}.`;
-    log.appendChild(dd);
-}
-
-function playerGame() {
-    const dd = document.createElement('dd');
-    dd.innerHTML = `You win! ${playerClicked} beats ${computerChoice}.`;
-    log.appendChild(dd);
+function gameResults(winner) {
+    if (winner == 'tie') {
+        const dd = document.createElement('dd');
+        dd.innerHTML = `It's a tie, you both played ${playerClicked}.`;
+        log.appendChild(dd);
+    }
+    if (winner == 'computer') {
+        const dd = document.createElement('dd');
+        dd.innerHTML = `You Lose! ${computerChoice} beats ${playerClicked}.`;
+        log.appendChild(dd);
+    }
+    if (winner == 'player') {
+        const dd = document.createElement('dd');
+        dd.innerHTML = `You win! ${playerClicked} beats ${computerChoice}.`;
+        log.appendChild(dd);
+    }
 }
 
 function compPlay() {
@@ -160,4 +175,13 @@ function compPlay() {
 function resetGame() {
     userScore = 0;
     computerScore = 0;
+}
+
+function resetRounds() {
+    userWins = 0;
+    computerWins = 0;
+    numberOfDraws = 0;
+    computer.innerHTML = '';
+    user.innerHTML = '';
+    drawGame.innerHTML = '';
 }
